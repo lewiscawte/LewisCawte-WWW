8eb83e610518bb9b383e630e5f57b705ea2dbad8
lcawte <lewis@lewiscawte.info>
2012-12-30 22:25:42 +0000

    Add some comments to explain code.

--- index.php
+++ index.php
@@ -5,18 +5,20 @@ $requestedPage = $_REQUEST['page'];
 if ( isset ( $requestedPage ) && !empty( $requestedPage ) ) {
 	$page = strtolower( $requestedPage );
 	if ( in_array( $page, $enabledPages ) ) {
+		// If requested page is in set configuration array, then return it.
 		$pageout = $page;
 	}
 	else {
+		// If requested page is not set in configuration array, return "error" page.
 		$pageout = "error";
 	}
 }
 else {
+	// If no page requested, return the index.
 	$pageout = "index";
 }
 require_once( "pageoutput.php" );
 ?>
-
 <!DOCTYPE>
 <html>
         <head>
--- pageoutput.php
+++ pageoutput.php
@@ -5,15 +5,20 @@ $outputTitle = "<title>Lewis Cawte :: Home | " . ucfirst( strtolower( $pageout )
 
 // Output the page content.
 if ( isset( $pagecontentPath ) ) {
+	// If page contents file path is set, carry on.
+	// Construct file path using directory information.
 	$contentsfile = $pagecontentPath . '/' . $pageout . '.txt';
+	// Check that the file exists.
 	if ( file_exists( $contentsfile ) ) {
 		$pagecontents = file_get_contents( $contentsfile );
 		$outputContent = $pagecontents;
 	}
 	else {
+		// Else throw an error saying file does not exist.
 		echo( '<div class="fatal-error-message">Error: Contents file does not exist for this page.</div>' );
 	}
 }
 else {
+	// If directory for page contents is not set, return an error.
 	echo( '<div class="fatal-error-message">Error: Content directory not set. Please contact the administrator referring $pagecontentPath</div>' );
 }
